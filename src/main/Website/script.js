0c2eab7666bf6a8f652a996a76f7bc332ce20324
Joe-Briefcase <s185143@student.dtu.dk>
2019-04-30 10:07:18 +0200

    JS

--- src/main/Website/script.js
+++ src/main/Website/script.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+   $("#adminPage").hide();
+   $("#editPage").hide();
+});
+
 $("#loginButton").click(function(){
     $("#loginPage").hide();
     $("#adminPage").show();
