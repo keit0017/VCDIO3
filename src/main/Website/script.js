b3fd669df1dfd884bf2e289f7c49b38adb5f7193
Joe-Briefcase <s185143@student.dtu.dk>
2019-04-30 10:51:15 +0200

    Begyndt på REST metoder.

--- src/main/Website/script.js
+++ src/main/Website/script.js
@@ -1,3 +1,7 @@
+/**
+ * Metoder til at vise og gemme elementer på siden.
+ */
+
 $("#loginButton").click(function(){
     $("#loginPage").hide();
     $("#adminPage").show();
@@ -11,4 +15,20 @@ $("#logOut").click(function(){
 $("#createUser").click(function(){
     $("#adminPage").hide();
     $("#editPage").show();
+});
+
+/**
+ * Metode til at hente data fra serveren.
+ */
+
+$("#loginButton").click(function () {
+   $("#userTable").load("URL");
+});
+
+/**
+ * Metode til at sende data til serveren når der skal oprettes bruger.
+ */
+
+$("#createUserButton").click(function () {
+    $.post("URL");
 });
